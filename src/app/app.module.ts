fb6305cf0c1fd50939d7a35b0a9dd45a14c2dd74
armaango <armaan.goyal@gmail.com>
2017-03-05 20:15:34 -0800

    connected to firebase app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes} from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,17 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListingComponent } from './components/listing/listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+
+import {FirebaseService} from './services/firebase.service';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDujkRSMEKJvvxtzat6EGMF7h5UGkn6gu8',
+  authDomain: 'proplistings-b1879.firebaseapp.com',
+  databaseURL: 'https://proplistings-b1879.firebaseio.com',
+  storageBucket: 'proplistings-b1879.appspot.com',
+  messagingSenderId: '62765117686'
+};
+
 
 const appRoutes:Routes = [
   {path:'',component:HomeComponent},
@@ -33,9 +45,10 @@ const appRoutes:Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
