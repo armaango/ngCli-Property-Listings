945c372c0ea291eebf987ce022861228f6a92b53
armaango <armaan.goyal@gmail.com>
2017-03-05 22:29:24 -0800

    logout routing fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +11,8 @@ import {FlashMessagesService} from 'angular2-flash-messages';
 export class NavbarComponent implements OnInit {
 
   constructor(public af:AngularFire,
-              public flashMessage:FlashMessagesService) { }
+              public flashMessage:FlashMessagesService,
+              public router:Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +22,7 @@ export class NavbarComponent implements OnInit {
   logout(){
     this.af.auth.logout();
     this.flashMessage.show('You are logged out',{cssClass:'alert-success', timeout:3000});
+    this.router.navigate(['/']);
   }
 
 }
